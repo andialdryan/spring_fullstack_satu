9bbc408f13b4af41636e07f5e62ccd554fceb100
andialdryan <aldryanmandala@gmail.com>
2023-12-13 23:04:07 +0700

    strategy in GeneratedValue

--- src/main/java/com/codewitharjun/fullstackbackend/model/User.java
+++ src/main/java/com/codewitharjun/fullstackbackend/model/User.java
@@ -2,13 +2,14 @@ package com.codewitharjun.fullstackbackend.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String name;
